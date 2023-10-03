2b9d30cd3e32d1cc2262f11d3dbc8bdc01d1d98a
Sreejith Suresh <125786861+Sreejith2003@users.noreply.github.com>
2023-10-03 11:33:39 +0530

    Add files via upload

---
+++ loc.py
@@ -0,0 +1,10 @@
+import phonenumbers
+from phonenumbers import carrier,timezone,geocoder
+number = input("Enter the number with + : ")
+phone = phonenumbers.parse(number)
+time = timezone.time_zones_for_number(phone)
+sim_name = carrier.name_for_number(phone,"en")
+region = geocoder.description_for_number(phone,"en")
+print("time_zone : ",time)
+print("Sim_name :",sim_name)
+print("Region : ",region)
